66c09295f49396a4b93eb388433543b1a9fad3d0
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-13 11:23:17 -0500

    Adjusted Game Board Design
    Instead of pasting 9 divs for the boxes, I've decided to generate them via array mapping.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,15 +1,22 @@
 document.addEventListener('click', (evt) => {
-    const { target } = evt;
-    if (target instanceof HTMLElement) {
-        console.log(evt.target.id)
-    }
+    console.log(evt)
 });
 
+let boxArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let boxes = boxArray.map((box) => {
+    console.log(box)
+    let classname = "board-box box-" + box;
+    return(
+        <div key={box} className={classname}></div>
+    )
+})
 
 function Board() {
     return(
         <div className="Board">
-            <div id="1" className="board-box box-1" data-value="1"></div>
+            {boxes}
+            {/* <div id="1" className="board-box box-1"></div>
             <div id="2" className="board-box box-2"></div>
             <div id="3" className="board-box box-3"></div>
             <div id="4"className="board-box box-4"></div>
@@ -17,7 +24,7 @@ function Board() {
             <div id="6" className="board-box box-6"></div>
             <div id="7" className="board-box box-7"></div>
             <div id="8" className="board-box box-8"></div>
-            <div id="9"className="board-box box-9"></div>
+            <div id="9"className="board-box box-9"></div> */}
         </div>
     )
 }
