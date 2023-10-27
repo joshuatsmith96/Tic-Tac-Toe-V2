b274de1d8704c9496e0693e1510a55c311701938
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-10-27 11:21:54 -0500

    Update Popup.tsx

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,14 +1,12 @@
 import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router'
 
 
 
 function Popup(){
-    const navigate = useNavigate();
     return(
         <div id="Popup">
             <h1 id="popup-h1">This person won!</h1>
-            <button onClick={() => {navigate(0)}}>New Game</button>
+            {/* <button onClick={() => {navigate(0)}}>New Game</button> */}
             <Link to="/Tic-Tac-Toe-V2" className="menu_button">Main Menu</Link>
         </div>
     )
